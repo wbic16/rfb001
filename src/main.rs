217817fdfc14bc9fbc020ddd5ca58b80f734823a
Will Bickford <wbic16@gmail.com>
2024-09-24 22:00:32 -0500

    Added leaders, scan, and index routes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use libphext::phext;
-use axum::{routing::get, Router};
+use axum::{routing::get, Router, response::Html};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     // Create a simple route handler
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/api/v1/leaders", get(leaders))
+        .route("/api/v1/scan", get(scan))
+    ;
 
     // Define the address to serve on
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -18,7 +22,18 @@ async fn main() {
         .unwrap();
 }
 
-// Simple handler function
-async fn handler() -> &'static str {
-    "Request for Bot #001"
+async fn index() -> Html<&'static str> {
+    Html("<html><head></head><body>
+    <ul>
+    <li><a href='/api/v1/leaders'>/api/v1/leaders</a></li>
+    <li><a href='/api/v1/scan'>/api/v1/scan</a></li>
+    </ul></body></html>")
+}
+
+async fn leaders() -> &'static str {
+    "@wbic16"
+}
+
+async fn scan() -> &'static str {
+    "TBD"
 }
